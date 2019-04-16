bd600cfb4f26de15b644c627521a816f5cc4b44e
OAghai <Ofir.Aghai@verint.com>
2019-04-16 15:18:06 +0300

    add default upload page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import { apiRouter } from "./api.routes";
+const fs = require('fs');
+const path = require('path');
 
 export class Routes {
   public routes(app): void {
     app.use("/api", apiRouter);
     app.route("/").get((req: Request, res: Response) => {
-      const pageContent: string = `<h1>PDF Heatmap 👑</h1>`;
-      res.status(200).send(pageContent);
+      console.log(__dirname)
+      res.sendFile('assets/upload.html', { root: path.join(__dirname, '../../public') });
     });
   }
 }
